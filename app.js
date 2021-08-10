df1fc0f786ca8fd3f811d6bc5ae32e5f61e212dd
yagizbabaozu <yagiz.babaozu@gmail.com>
2021-08-10 16:29:49 +0300

    javascript map method added

--- app.js
+++ app.js
@@ -34,11 +34,14 @@ app.controller("getData", function ($scope, $http) {
   }).then(function (response) {
     $scope.entries = response.data.entries;
     let releaseYear2010 = [];
-    for (i = 0; i < response.data.total; i++) {
+    response.data.entries.map(
+      (item) => item.releaseYear > 2010 && releaseYear2010.push(item)
+    );
+    /* for (i = 0; i < response.data.total; i++) {
       if (response.data.entries[i].releaseYear > 2010) {
         releaseYear2010.push(response.data.entries[i]);
       }
-    }
+    } */
     $scope.releaseYear2010 = releaseYear2010;
     $scope.loading = false;
   });
