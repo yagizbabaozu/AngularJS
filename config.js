fde7fe2430480e60088789a284d902a5da5be1dc
yagizbabaozu <yagiz.babaozu@gmail.com>
2021-08-10 13:00:08 +0300

    config fixed

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 app.config(function ($routeProvider) {
+  $routeProvider.when("/", {
+    templateUrl: "components/home/home.html",
+  });
+});
+
+app.config(function ($routeProvider) {
   $routeProvider.when("/home", {
     templateUrl: "components/home/home.html",
   });
